ff4a9b56f226cbba30c6c6a957701f6fcc09bf1e
Ismael Souza <ismael.souza@equals.com.br>
2020-08-19 11:56:34 -0300

    Inclusão da classe de atualização das informações e padronização dos dados

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,28 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class UpdateData(BaseEstimator, TransformerMixin):
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        mean_for_ok = 7.0
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data.loc[(data["PERFIL"] == "DIFICULDADE") & (data["REPROVACOES_DE"] == 0), "REPROVACOES_DE"] = 1
+        data.loc[(data["PERFIL"] == "DIFICULDADE") & (data["REPROVACOES_EM"] == 0), "REPROVACOES_EM"] = 1
+        data.loc[(data["PERFIL"] == "DIFICULDADE") & (data["REPROVACOES_MF"] == 0), "REPROVACOES_MF"] = 1
+        data.loc[(data["PERFIL"] == "DIFICULDADE") & (data["REPROVACOES_GO"] == 0), "REPROVACOES_GO"] = 1
+        data.loc[(data["PERFIL"] == "EXATAS") & (data["REPROVACOES_MF"] == 0) & (data["NOTA_MF"] < mean_for_ok), "REPROVACOES_MF"] = 1
+        data.loc[(data["PERFIL"] == "EXATAS") & (data["REPROVACOES_EM"] == 0) & (data["NOTA_EM"] < mean_for_ok), "REPROVACOES_EM"] = 1
+        data.loc[(data["PERFIL"] == "HUMANAS") & (data["REPROVACOES_EM"] == 0) & (data["NOTA_EM"] < mean_for_ok), "REPROVACOES_MF"] = 1
+        data.loc[(data["PERFIL"] == "HUMANAS") & (data["REPROVACOES_GO"] == 0) & (data["NOTA_GO"] < mean_for_ok), "REPROVACOES_EM"] = 1
+        data.loc[(df_data_1["PERFIL"] == "EXCELENTE") & (data["NOTA_DE"] == 0), "NOTA_DE"] = 10
+        data.loc[(df_data_1["PERFIL"] == "EXCELENTE") & (data["NOTA_EM"] == 0), "NOTA_EM"] = 10
+        data.loc[(df_data_1["PERFIL"] == "EXCELENTE") & (data["NOTA_MF"] == 0), "NOTA_MF"] = 10
+        data.loc[(df_data_1["PERFIL"] == "EXCELENTE") & (data["NOTA_GO"] == 0), "NOTA_GO"] = 10
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
