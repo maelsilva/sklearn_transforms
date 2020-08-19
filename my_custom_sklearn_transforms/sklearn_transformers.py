ece93b064bdfba2014005ad7494c31b14d506b6d
Ismael Souza <ismael.souza@equals.com.br>
2020-08-19 15:08:09 -0300

    Teste

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -26,22 +26,22 @@ class UpdateData(BaseEstimator, TransformerMixin):
         data = X.copy()
         data.loc[
             (data["PERFIL"] == "DIFICULDADE") 
-            & ((data["REPROVACOES_DE"] == 0 | data["REPROVACOES_DE"].empty))
+            & (((data["REPROVACOES_DE"] == 0) | (data["REPROVACOES_DE"].empty)))
             , "REPROVACOES_DE"
         ] = 1
         data.loc[
             (data["PERFIL"] == "DIFICULDADE")
-            & ((data["REPROVACOES_EM"] == 0 | data["REPROVACOES_EM"].empty))
+            & (((data["REPROVACOES_EM"] == 0) | (data["REPROVACOES_EM"].empty)))
             , "REPROVACOES_EM"
         ] = 1
         data.loc[
             (data["PERFIL"] == "DIFICULDADE") 
-            & ((data["REPROVACOES_MF"] == 0 | data["REPROVACOES_MF"].empty))
+            & (((data["REPROVACOES_MF"] == 0) | (data["REPROVACOES_MF"].empty)))
             , "REPROVACOES_MF"
         ] = 1
         data.loc[
             (data["PERFIL"] == "DIFICULDADE") 
-            & ((data["REPROVACOES_GO"] == 0 | data["REPROVACOES_GO"].empty))
+            & (((data["REPROVACOES_GO"] == 0) | (data["REPROVACOES_GO"].empty)))
             , "REPROVACOES_GO"
         ] = 1
         data.loc[
@@ -51,37 +51,37 @@ class UpdateData(BaseEstimator, TransformerMixin):
         ] = 1
         data.loc[
             (data["PERFIL"] == "EXATAS") 
-            & ((data["REPROVACOES_EM"] == 0 | data["REPROVACOES_EM"].empty) & (data["NOTA_EM"] < mean_for_ok | data["NOTA_EM"].empty)) 
+            & (((data["REPROVACOES_EM"] == 0) | (data["REPROVACOES_EM"].empty)) & ((data["NOTA_EM"] < mean_for_ok) | (data["NOTA_EM"].empty))) 
             , "REPROVACOES_EM"
         ] = 1
         data.loc[
             (data["PERFIL"] == "HUMANAS") 
-            & ((data["REPROVACOES_DE"] == 0 | data["REPROVACOES_DE"].empty) & (data["NOTA_DE"] < mean_for_ok | data["NOTA_DE"].empty)) 
+            & (((data["REPROVACOES_DE"] == 0) | (data["REPROVACOES_DE"].empty)) & ((data["NOTA_DE"] < mean_for_ok) | (data["NOTA_DE"].empty))) 
             , "REPROVACOES_DE"
         ] = 1
         data.loc[
             (data["PERFIL"] == "HUMANAS") 
-            & ((data["REPROVACOES_GO"] == 0 | data["REPROVACOES_GO"].empty) & (data["NOTA_GO"] == mean_for_ok | data["NOTA_GO"].empty))
+            & (((data["REPROVACOES_GO"] == 0) | (data["REPROVACOES_GO"].empty)) & ((data["NOTA_GO"] == mean_for_ok) | (data["NOTA_GO"].empty)))
             , "REPROVACOES_GO"
         ] = 1
         data.loc[
             (df_data_1["PERFIL"] == "EXCELENTE") 
-            & ((data["NOTA_DE"] == mean_for_ok | data["NOTA_DE"].empty)) 
+            & (((data["NOTA_DE"] == mean_for_ok) | (data["NOTA_DE"].empty))) 
             , "NOTA_DE"
         ] = 10
         data.loc[
             (df_data_1["PERFIL"] == "EXCELENTE") 
-            & ((data["NOTA_EM"] == mean_for_ok | data["NOTA_EM"].empty)) 
+            & (((data["NOTA_EM"] == mean_for_ok) | (data["NOTA_EM"].empty))) 
             , "NOTA_EM"
         ] = 10
         data.loc[
             (df_data_1["PERFIL"] == "EXCELENTE") 
-            & ((data["NOTA_MF"] == mean_for_ok | data["NOTA_MF"].empty)) 
+            & (((data["NOTA_MF"] == mean_for_ok) | (data["NOTA_MF"].empty))) 
             , "NOTA_MF"
         ] = 10
         data.loc[
             (df_data_1["PERFIL"] == "EXCELENTE") 
-            & ((data["NOTA_GO"] == mean_for_ok | data["NOTA_GO"].empty)) 
+            & (((data["NOTA_GO"] == mean_for_ok) | (data["NOTA_GO"].empty))) 
             , "NOTA_GO"
         ] = 10
         # Retornamos um novo dataframe sem as colunas indesejadas
